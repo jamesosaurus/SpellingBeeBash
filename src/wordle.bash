59e865f29ae95d21142c8e41a19e8f7f63616e9f
James Riordan <james.riordan@gmail.com>
2024-07-22 07:28:40 -0400

    rename wordlist.txt to list.txt

--- src/wordle.bash
+++ src/wordle.bash
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!bash
 
-echo $(echo -n grep  ^$1\$ wordlist.txt \| egrep -v \[_$2\]; for item in ${@:3} ; do echo -n  \| grep $item; done) | bash
+echo $(echo -n grep  ^$1\$ list.txt \| egrep -v \[_$2\]; for item in ${@:3} ; do echo -n  \| grep $item; done) | bash
 
 
 

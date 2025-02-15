a429ac752cb568480219756846df11fc08f5cfa0
James Riordan <james.riordan@gmail.com>
2025-02-15 18:42:22 -0500

    strands

---
+++ src/strands.bash
@@ -0,0 +1,17 @@
+function normalize() {
+
+  echo $1 | fold -w1 | sort | fmt | sed -e s/" "//g
+
+}
+
+ 
+
+target=$(normalize $1)
+
+#echo $n
+
+ 
+
+for word in $(egrep -v [$(echo {a..z}| sed  -e s/" "//g -e s/[$1]//g)] wordlist.txt); do
+    if [ "$(normalize $word)" == "$target" ]; then echo $word; fi
+done
